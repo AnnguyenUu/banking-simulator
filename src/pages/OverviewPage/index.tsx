c65b18fc74895220f869c81ba4ce252ca01b4c82
AnnguyenUu <annguyenthanh292@gmail.com>
2026-07-06 15:08:50 +0700

    Fix

--- src/pages/OverviewPage/index.tsx
+++ src/pages/OverviewPage/index.tsx
@@ -13,9 +13,9 @@ const RecentTransactions = lazy(
   () => import("./components/RecentTransactions"),
 );
 
-const getTotalBalance = (accounts: Account[]) =>  {
+const getTotalBalance = (accounts: Account[]) => {
   return accounts?.reduce((sum, acc) => sum + acc.balance, 0) ?? 0;
-}
+};
 
 export function OverviewPage() {
   const { accounts, isLoading: accountsLoading } = useAccounts();
@@ -25,7 +25,7 @@ export function OverviewPage() {
     perPage: 5,
   });
 
-  const totalBalance: number = getTotalBalance(accounts)
+  const totalBalance: number = getTotalBalance(accounts);
 
   return (
     <PageLayout title="Overview">
